2f62734b1f192e42be359b7aa14fd58d7d447f20
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-07-26 22:14:28 +0900

    経路プロバイダを使って歩くように

--- src/style/MeleeStyleTrait.php
+++ src/style/MeleeStyleTrait.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Echore\NaturalEntity\style;
 
 use Echore\NaturalEntity\INaturalEntity;
+use Echore\NaturalEntity\utils\VectorUtil;
 use pocketmine\entity\Living;
+use pocketmine\math\Vector3;
 
 trait MeleeStyleTrait {
 
@@ -27,7 +31,12 @@ trait MeleeStyleTrait {
 			$this->setRotation($this->getLocation()->getYaw(), 0);
 		}
 
-		$this->walkForward();
+		$resultDirection = $this->walkTo($this->getInstanceTarget()->getPosition()->add(0, 0.5, 0), true);
+
+		if (!is_null($resultDirection)) {
+			$horizontal = VectorUtil::getAngle($this->getPosition(), $this->getPosition()->addVector(new Vector3($resultDirection->x, 0, $resultDirection->y)));
+			$this->setRotation($horizontal->x, 0);
+		}
 
 		$this->tryAttackEntity($this->getInstanceTarget());
 	}
